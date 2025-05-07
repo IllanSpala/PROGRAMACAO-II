b610083437ea0ef7fd095cd530ad53724bda33a4
IllanSpala <illanspala@github.com>
2025-05-07 14:52:01 -0300

    Initial commit - Uploading PROG II files

---
+++ Main.java
@@ -0,0 +1,107 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;  
+
+
+
+public class Main{
+/* EXERCICIO 2
+public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+	    
+	    int n1, n2, resto;
+	    System.out.println("Whats the first Number?");
+	    n1 = scanner.nextInt();
+	    System.out.println("Whats the second Number?");
+	    n2 = scanner.nextInt();
+	    resto = n1 % n2;
+	    
+	    System.out.println("N1:" + n1);
+	    System.out.println("N2:" + n2);
+	    System.out.println("Resto:" + resto);
+	}
+*/
+/* EXERCICIO 3
+    public static void main(String[] args) {
+    int op1, op2, op3;
+    op1 = -5 + 7 * 8;
+    op2 = 72 + 9 % 9;
+    op3 = 5 + (15 / 3) * 3 - 4 % 3;
+    System.out.println("-5 + 7 * 8 = " + op1);
+    System.out.println("72 + 9 % 9 = " + op2);
+    System.out.println("5 + (15 / 3) * 3 - 4 % 3 = " + op3);
+    }
+*/
+/* EXERCICIO 4
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+            int v1, v2;
+        
+        System.out.println("Whats the first number?");
+        v1 = scanner.nextInt();
+        System.out.println("Whats the second number?");
+        v2 = scanner.nextInt();
+        
+        if(v1 == v2){
+            System.out.println("The number "+ v1 +" is equal to " + v2);
+        }else if(v1 > v2){
+            System.out.println("The number "+ v1 +" is bigger than " + v2);
+        }else{
+            System.out.println("The number "+ v1 +" is smaller than " + v2);
+        }
+    }
+*/
+/* EXERCICIO 5
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        int espaço, i;
+        System.out.print("Whats the space u wanna to check in?");
+        espaço = scanner.nextInt();
+        
+        for(i = 1; i <= espaço; i++){
+            if(i % 2 != 0){
+                System.out.println(i);
+                i++;
+            }else{
+                i++;
+            }
+        } 
+    }
+*/
+/* EXERCICIO 6
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String n1, n2, nConc;
+        
+        System.out.print("Whats ur first name: ");
+        n1 = scanner.next();
+        System.out.print("Whats ur second name: ");
+        n2 = scanner.next();
+        nConc = (n1 +" "+ n2);
+        System.out.print("Concatenated name: " + nConc);
+        
+    }
+*/
+/*EXERCICIO 7
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        double n1, n2, result;
+        
+        System.out.println("first number:");
+        n1 = scanner.nextDouble();
+        System.out.println("second number:");
+        n2 = scanner.nextDouble();
+        
+        result =(n1+n2);
+        
+        System.out.println("Results = " + result)
+    }
+*/
+} 
